777a9bd4cf4b3ac6d53147184cca3dab4b268340
abdullah-patwary <abdullahpatwary2019@gmail.com>
2022-11-10 23:20:47 +0600

    Solve the division problem

---
+++ Python Division.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(int(a/b)) # This statement print the result  as integer
+print((a/b)) # This statement print the result as string
