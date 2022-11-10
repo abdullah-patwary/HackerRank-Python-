9573c79d5e88b787894720f98acc5e7d467ed644
abdullah-patwary <abdullahpatwary2019@gmail.com>
2022-11-10 23:33:02 +0600

    Solve the problem of loop from hackerrank.com

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HackerRank-Python-
+Hello Everyone,
+This is Abdullah. In this repository, I'm trying to solve some python problems from "hackerrank.com".
---
+++ python Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(n): #The value of i will increment with the logic: i<n
+    print(i*i) #This line print the result of i^2 with newline
